726dfe014fe9d9a77b4937673211d892998f760e
Backxtar <f358145d>
2021-09-25 11:46:15 +0200

    Added conn timeout

--- src/main/java/de/backxtar/gw2/Gw2Utils.java
+++ src/main/java/de/backxtar/gw2/Gw2Utils.java
@@ -34,6 +34,8 @@ public class Gw2Utils {
     private static InputStream read(URL url) throws IOException {
         HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
         httpCon.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+        httpCon.setConnectTimeout(10000);
+        httpCon.setReadTimeout(10000);
         return httpCon.getInputStream();
     }
 
