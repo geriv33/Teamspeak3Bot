dd9340a867b69cb4fb482f004a74db7bd86fc520
Backxtar <f358145d>
2021-09-27 22:23:52 +0200

    Added TimerName to Timer System.

--- src/main/java/de/backxtar/commands/manage/HelpCommand.java
+++ src/main/java/de/backxtar/commands/manage/HelpCommand.java
@@ -16,9 +16,9 @@ public class HelpCommand implements CommandInterface {
                 "╰ [color=" + colors.mainColor + "][b]!key yourAPIKey[/b][/color] ⎯  Speichert oder aktualisiert Deinen [b]Gw2-Key[/b]\n" +
                 "╰ [color=" + colors.mainColor + "][b]!keyremove[/b][/color] ⎯  Löscht Deinen [b]Gw2-Key[/b]\n" +
                 "╰ [color=" + colors.mainColor + "][b]!uid[/b][/color] ⎯  Zeigt Dir Deine [b]UID[/b] an\n" +
-                "╰ [color=" + colors.mainColor + "][b]!timer 1h,2m | 11.11.2011 13:30[/b][/color] ⎯  Startet einen [b]Timer[/b]\n" +
+                "╰ [color=" + colors.mainColor + "][b]!timer 1h,2m name | 11.11.2011-13:30 name[/b][/color] ⎯  Startet einen [b]Timer[/b]\n" +
                 "╰ [color=" + colors.mainColor + "][b]!timers[/b][/color] ⎯  Zeigt Dir Deine [b]Timer[/b] an\n" +
-                "╰ [color=" + colors.mainColor + "][b]!cancel timerID[/b][/color] ⎯  Bricht Deinen [b]Timer[/b] ab\n" +
+                "╰ [color=" + colors.mainColor + "][b]!cancel timer[/b][/color] ⎯  Bricht Deinen [b]Timer[/b] ab\n" +
                 "╰ [color=" + colors.mainColor + "][b]!me[/b][/color] ⎯  Gibt Dir Deine [b]Gw2-Accountinformationen[/b] aus\n" +
                 "╰ [color=" + colors.mainColor + "][b]!wallet[/b][/color] ⎯  Zeigt Dir Deine [b]Gw2-Geldbörse[/b] an\n" +
                 "╰ [color=" + colors.mainColor + "][b]!bosses[/b][/color] ⎯  Zeigt Dir Deine [b]Gw2-Raid-Überischt[/b] an\n" +
